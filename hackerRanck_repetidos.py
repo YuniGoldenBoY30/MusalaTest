d1c620cd440eaffb8fe0f52371b892565997d566
Yuniel <yuni.boss@gmail.com>
2022-08-06 01:09:31 -0400

    script ejercicios de hackerrank para musalaSoft

---
+++ hackerRanck_repetidos.py
@@ -0,0 +1,29 @@
+def print_repetido(name):
+    # Use a breakpoint in the code line below to debug your script.
+    l = list(name)
+    cadena = ''
+    c = 1
+    for (k, v) in enumerate(name):
+        li = l[:k + 1]
+        if len(li) == 1:
+            cadena += v
+        if li[k] == li[k - 1] and len(li) != 1:
+            c += 1
+        if li[k] == li[k - 1] and len(li) == len(name):
+            cadena += str(c)
+            break
+        if li[k] != li[k - 1] or len(li) == len(name):
+            if c > 1:
+                cadena += str(c)
+                cadena += v
+                c = 1
+            else:
+                cadena += v
+
+print(cadena)
+
+if __name__ == '__main__':
+    print_repetido('abbcc')
+
+#input abbcc
+#output ab2c2
